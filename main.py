0972ffad24e22c99bd6655d848726a665ee2485f
guillochon <guillochon@gmail.com>
2016-07-30 10:01:57 -0400

    BUG: updated main to use `ArgsHandler`

--- main.py
+++ main.py
@@ -4,36 +4,19 @@
 
 def main(args, clargs, log):
     from .tidaldisruptioncatalog import TidalDisruptionCatalog
+    from astrocats.catalog.argshandler import ArgsHandler
 
-    # Load tidal disruption-specific command-line argumenets
-    # (adding them to existing settings)
-    args = load_command_line_args(args=args, clargs=clargs)
+    # Create an `ArgsHandler` instance with the appropriate argparse machinery
+    args_handler = ArgsHandler(log)
+    # Parse the arguments to get the configuration settings
+    args = args_handler.load_args(args=args, clargs=clargs)
+    # Returns 'None' if no subcommand is given
     if args is None:
         return
 
     catalog = TidalDisruptionCatalog(args, log)
 
-    if args.subcommand == 'import':
-        log.info("Running `importer`.")
-        catalog.import_data()
+    # Run the subcommand given in `args`
+    args_handler.run_subcommand(args, catalog)
 
     return
-
-
-def load_command_line_args(args=None, clargs=None):
-    """Load and parse command-line arguments.
-    """
-    import argparse
-    from astrocats.catalog.main import add_parser_arguments
-
-    parser = argparse.ArgumentParser(prog='tidaldisruptions',
-                                     description='The Open Tidal Disruption '
-                                     'Event Catalog.')
-
-    subparsers = parser.add_subparsers(
-        description='valid subcommands', dest='subcommand')
-    # `import` --- importing tidal disruption data
-    import_pars = subparsers.add_parser("import",
-                                        help="Import tidal disruption data.")
-
-    return add_parser_arguments(parser, import_pars, args, clargs)
