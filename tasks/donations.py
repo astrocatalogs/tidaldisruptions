3cc5a994b0ed87b86936f86022041b03b1aa8a11
guillochon <guillochon@gmail.com>
2016-09-23 07:03:19 -0700

    ENH: added Auchettl-2016 meta to donations

--- tasks/donations.py
+++ tasks/donations.py
@@ -5,6 +5,7 @@ import os
 
 from astrocats.catalog.photometry import PHOTOMETRY
 from astrocats.catalog.utils import pbar
+from astrocats.tidaldisruptions.tidaldisruption import TIDALDISRUPTION
 from cdecimal import Decimal
 
 
@@ -17,7 +18,7 @@ def do_donations(catalog):
     with open(datafile, 'r') as f:
         tsvin = csv.reader(f, delimiter=',', skipinitialspace=True)
 
-        for ri, row in enumerate(pbar(tsvin, task_str)):
+        for ri, row in enumerate(pbar(list(tsvin), task_str)):
             if ri <= 3 or not row or not row[0]:
                 continue
             (name, source) = catalog.new_entry(
@@ -51,4 +52,31 @@ def do_donations(catalog):
             catalog.entries[name].add_photometry(**photodict)
 
     catalog.journal_entries()
+
+    datafile = os.path.join(catalog.get_current_task_repo(), 'Donations',
+                            'Auchettl-2016-meta.csv')
+    with open(datafile, 'r') as f:
+        tsvin = csv.reader(f, delimiter=',', skipinitialspace=True)
+
+        for ri, row in enumerate(pbar(list(tsvin), task_str)):
+            if ri <= 2 or not row or not row[0]:
+                continue
+            (name, source) = catalog.new_entry(
+                row[0].strip(), srcname='Open TDE Catalog')
+            sources = [source]
+            for new_src in [x.strip() for x in row[7].split(',')]:
+                sources.append(catalog.entries[name].add_source(
+                    bibcode=new_src))
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.HOST, row[1].strip(), source=source)
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.HOST_RA, row[2].strip(), source=source)
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.HOST_DEC, row[3].strip(), source=source)
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.RA, row[4].strip(), source=source)
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.DEC, row[5].strip(), source=source)
+            catalog.entries[name].add_quantity(
+                TIDALDISRUPTION.REDSHIFT, row[6].strip(), source=source)
     return
