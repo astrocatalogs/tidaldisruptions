eddf07dfef8c70e6061ed3943170bc465fd87002
guillochon <guillochon@gmail.com>
2017-02-14 14:03:55 -0800

    ENH: updated script to pull just before push

--- scripts/auto-update-catalog.sh
+++ scripts/auto-update-catalog.sh
@@ -5,11 +5,13 @@ PATH=/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
 cd /var/www/html/tde/astrocats
+python3.5 -m astrocats tidaldisruptions git-pull
 python3.5 -m astrocats tidaldisruptions import
 SNEUPDATE=$?
 echo $SNEUPDATE
 if [[ $SNEUPDATE == 0 ]]; then
 	astrocats/tidaldisruptions/scripts/generate-web.sh
+	python3.5 -m astrocats tidaldisruptions git-pull
 	python3.5 -m astrocats tidaldisruptions git-push
 	#stamp=$(date +"%Y-%m-%d %k:%M")
 	#./commit-and-push-repos.sh "Auto update: $stamp"
