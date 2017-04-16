6338e81747200b8e2daddae6fe49424851cf4452
guillochon <guillochon@gmail.com>
2017-04-15 20:29:46 -0700

    MAINT: changed python3.5 to just python in scripts.

--- scripts/generate-web.sh
+++ scripts/generate-web.sh
@@ -5,25 +5,25 @@ PATH=/opt/local/bin:/usr/local/bin:$PATH ; export PATH
 LD_LIBRARY_PATH=/usr/local/lib:/opt/local/lib ; export LD_LIBRARY_PATH
 
 cd /var/www/html/tde/astrocats
-python3.5 -m astrocats.scripts.webcat -c tde &
+python -m astrocats.scripts.webcat -c tde &
 pids[0]=$!
-python3.5 -m astrocats.scripts.webcat -c tde -by &
+python -m astrocats.scripts.webcat -c tde -by &
 pids[1]=$!
-#python3.5 -m astrocats.tidaldisruptions.scripts.dupecat &
+#python -m astrocats.tidaldisruptions.scripts.dupecat &
 #$pids[2]=$!
-#python3.5 -m astrocats.tidaldisruptions.scripts.conflictcat &
+#python -m astrocats.tidaldisruptions.scripts.conflictcat &
 #$pids[3]=$!
-python3.5 -m astrocats.scripts.bibliocat -c tde &
+python -m astrocats.scripts.bibliocat -c tde &
 pids[2]=$!
-#python3.5 -m astrocats.tidaldisruptions.scripts.erratacat &
+#python -m astrocats.tidaldisruptions.scripts.erratacat &
 #$pids[5]=$!
-python3.5 -m astrocats.scripts.hostcat -c tde &
+python -m astrocats.scripts.hostcat -c tde &
 pids[3]=$!
-python3.5 -m astrocats.scripts.hammertime -c tde &
+python -m astrocats.scripts.hammertime -c tde &
 pids[4]=$!
-#python3.5 -m astrocats.tidaldisruptions.scripts.histograms &
+#python -m astrocats.tidaldisruptions.scripts.histograms &
 #$pids[8]=$!
-python3.5 -m astrocats.scripts.atelscbetsiaucs -c tde &
+python -m astrocats.scripts.atelscbetsiaucs -c tde &
 pids[5]=$!
 for pid in ${pids[*]}; do
 	wait $pid
