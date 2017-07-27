46c6a02c549f02a4bc3e1940ac56a5c17db1a9af
guillochon <guillochon@gmail.com>
2017-07-27 18:27:57 -0400

    MAINT: remove VV ASCII donation

--- tasks/ascii.py
+++ tasks/ascii.py
@@ -110,50 +110,5 @@ def do_ascii(catalog):
             }
             catalog.entries[name].add_photometry(**photodict)
 
-    # 2011ApJ...741...73V
-    file_path = os.path.join(catalog.get_current_task_repo(), 'ASCII',
-                             '2011ApJ...741...73V.txt')
-    with open(file_path, 'r') as f:
-        data = list(
-            csv.reader(
-                f, delimiter=' ', quotechar='"', skipinitialspace=True))
-        bandcols = {}
-        ebandcols = {}
-        limitcols = {}
-        for r, row in enumerate(pbar(data, task_str)):
-            if r == 0:
-                for ci, col in enumerate(row):
-                    if col.startswith('MAG_'):
-                        bandcols[ci] = col.replace('MAG_', '')
-                    elif col.startswith('MAGERR_'):
-                        ebandcols[col.replace('MAGERR_', '')] = ci
-                    elif col.startswith('MAGLIM_'):
-                        limitcols[col.replace('MAGLIM_', '')] = ci
-                continue
-            elif row[0][0] == '#':
-                name, source = catalog.new_entry(
-                    row[0].lstrip('#'), bibcode='2011ApJ...741...73V')
-                continue
-            mjd = row[0]
-            for ci, col in enumerate(row):
-                if ci in bandcols:
-                    e_mag = str(-Decimal(row[ebandcols[bandcols[ci]]]))
-                    if float(e_mag) < 0.0 or float(col) < 0.0:
-                        mag = limitcols[bandcols[ci]]
-                        e_mag = ''
-                        upplim = True
-                    else:
-                        mag = col
-                        upplim = False
-                    photodict = {
-                        PHOTOMETRY.TIME: mjd,
-                        PHOTOMETRY.BAND: bandcols[ci],
-                        PHOTOMETRY.MAGNITUDE: mag,
-                        PHOTOMETRY.E_MAGNITUDE: e_mag,
-                        PHOTOMETRY.UPPER_LIMIT: upplim,
-                        PHOTOMETRY.SOURCE: source
-                    }
-                    catalog.entries[name].add_photometry(**photodict)
-
     catalog.journal_entries()
     return
